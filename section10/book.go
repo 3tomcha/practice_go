be81b0b1e058e1401c44b60127bf9219858ccb86
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-09-25 10:57:57 +0900

    book validation

---
+++ section10/book.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type Book struct {
+	Title string `validate:"required"`
+	Prize string `validate:"required"`
+}
+
+func main() {
+	s := `{"Title":"Real World HTTP ミニ版", "Price":0}`
+	var b Book
+	if err := json.Unmarshal([]byte(s), &b); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := validator.New().Struct(b); err != nil {
+		var ve validator.ValidationErrors
+		if errors.As(err, &ve) {
+			for _, fe := range ve {
+				fmt.Printf("フィールド %s が %s 違反です（値:%v）\n", fe.Field(), fe.Tag())
+			}
+		}
+	}
+}
